86cd912b120d5406e8b62a4bdd49923cab774e00
dev_olagold <olagoldlanre@gmail.com>
2022-12-13 04:31:45 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@ const toggleBtn = document.querySelector(".toggle-button");
 const navLink = document.querySelector(".nav-link");
 const ctaButton = document.querySelector(".butn-button");
 const navbar = document.querySelector(".navbar");
-const heroText1 = document.querySelector(".hero-text-1")
-const heroText2 = document.querySelector(".hero-text-2")
-const heroText3 = document.querySelector(".hero-text-3")
+const year = document.querySelector(".year");
 
 toggleBtn.addEventListener('click', () =>{
     navLink.classList.toggle('active')
@@ -13,15 +11,6 @@ toggleBtn.addEventListener('click', () =>{
     toggleBtn.classList.toggle('open')
 })
 
+const currentYear = new Date().getFullYear()
 
-// document.querySelector(".hero-button").addEventListener("click", ()=>{
-//     heroText.classList.toggle("gradient")
-// })
-
-function addGradient(){
-    heroText1.classList.toggle("gradient")
-    heroText2.classList.toggle("gradient")
-    // heroText3.classList.toggle("gradient")
-}
-
-// setInterval(addGradient, 4000);
+year.innerHTML = currentYear
